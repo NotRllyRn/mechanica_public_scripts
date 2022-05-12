98713baf292a3955eda2f2194c8bc6642d94c79e
NotRllyRn <75143746+NotRllyRn@users.noreply.github.com>
2022-05-12 00:23:18 -0700

    Create AutoSave.lua

---
+++ AutoSave.lua
@@ -0,0 +1,45 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait() 
+end
+
+local Client = game.Players.LocalPlayer
+local GUI = Client:WaitForChild("PlayerGui")
+
+while GUI:FindFirstChild("LoadingScreenGui∙") or not GUI:FindFirstChild("MainGui") do
+    wait()
+end
+
+wait(3)
+
+local Save24 = GUI.MainGui.DespawnedUI.SideMenu.SavesBox.Saves["24"]
+local Save24Name = GUI.MainGui.SavingValues["CreationName 24"]
+local Spawned = GUI.MainGui.Values.Spawned
+
+Save24Name.Value = "AUTOSAVE"
+Save24.SaveLoad:FireServer("Load")
+
+local timeoutTable = {}
+local function timeout(callback)
+    for _,v in ipairs(timeoutTable) do
+        v[1] = false
+    end
+
+    task.spawn(function()
+        local t = { true }
+        table.insert(timeoutTable, t)
+        wait(10)
+        if t[1] ~= false then
+            callback()
+        end
+
+        table.remove(timeoutTable, table.find(timeoutTable, t))
+    end)
+end
+
+workspace.Creations.DescendantAdded:Connect(function(v)
+    if v.Parent.Name == Client.Name and not Spawned.Value then
+        timeout(function()
+            Save24.SaveLoad:FireServer("Save")
+        end)
+    end
+end)
